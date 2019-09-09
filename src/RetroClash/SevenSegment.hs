bd6894bf7fdcf477741dc9d3705e813d1ace7c68
Dr. ERDI Gergo <gergo@erdi.hu>
2019-09-09 15:56:03 +0200

    More dense ASCII art rendering of SS digits

--- src/RetroClash/SevenSegment.hs
+++ src/RetroClash/SevenSegment.hs
@@ -71,17 +71,17 @@ encodeHexSS n = case n of
 
 showSS :: Vec 7 Bool -> String
 showSS (a :> b :> c :> d :> e :> f :> g :> Nil) = unlines . L.concat $
-    [ L.replicate 1 $ horiz a
-    , L.replicate 4 $ vert f b
-    , L.replicate 1 $ horiz g
-    , L.replicate 4 $ vert e c
-    , L.replicate 1 $ horiz d
+    [ L.replicate 1 $ horiz   a
+    , L.replicate 3 $ vert  f   b
+    , L.replicate 1 $ horiz   g
+    , L.replicate 3 $ vert  e   c
+    , L.replicate 1 $ horiz   d
     ]
   where
-    horiz True  = "  #######  "
-    horiz False = "  .......  "
+    horiz True  = " ###### "
+    horiz False = " ...... "
 
-    vert b1 b2 = part b1 <> "       " <> part b2
+    vert b1 b2 = part b1 <> "      " <> part b2
       where
-        part True  = "##"
-        part False = ".."
+        part True  = "#"
+        part False = "."
