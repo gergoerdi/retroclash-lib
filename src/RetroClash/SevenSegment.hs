e22e0cefd85cc218a07d17fa12facd56c0ba9799
Dr. ERDI Gergo <gergo@erdi.hu>
2019-09-19 10:55:32 +0200

    #ignore formatting

--- src/RetroClash/SevenSegment.hs
+++ src/RetroClash/SevenSegment.hs
@@ -53,22 +53,22 @@ bytesSS div bytes = (shownDigit, segments)
 encodeHexSS :: Unsigned 4 -> Vec 7 Bool
 encodeHexSS n = case n of
     --       a        b        c        d        e        f        g
-    0x0 ->   True :>  True :>  True :>  True :>  True :>  True :> False :> Nil
-    0x1 ->  False :>  True :>  True :> False :> False :> False :> False :> Nil
-    0x2 ->   True :>  True :> False :>  True :>  True :> False :>  True :> Nil
-    0x3 ->   True :>  True :>  True :>  True :> False :> False :>  True :> Nil
-    0x4 ->  False :>  True :>  True :> False :> False :>  True :>  True :> Nil
-    0x5 ->   True :> False :>  True :>  True :> False :>  True :>  True :> Nil
-    0x6 ->   True :> False :>  True :>  True :>  True :>  True :>  True :> Nil
-    0x7 ->   True :>  True :>  True :> False :> False :> False :> False :> Nil
-    0x8 ->   True :>  True :>  True :>  True :>  True :>  True :>  True :> Nil
-    0x9 ->   True :>  True :>  True :>  True :> False :>  True :>  True :> Nil
-    0xa ->   True :>  True :>  True :> False :>  True :>  True :>  True :> Nil
-    0xb ->  False :> False :>  True :>  True :>  True :>  True :>  True :> Nil
-    0xc ->   True :> False :> False :>  True :>  True :>  True :> False :> Nil
-    0xd ->  False :>  True :>  True :>  True :>  True :> False :>  True :> Nil
-    0xe ->   True :> False :> False :>  True :>  True :>  True :>  True :> Nil
-    0xf ->   True :> False :> False :> False :>  True :>  True :>  True :> Nil
+    0x0 ->  True  :> True  :> True  :> True  :> True  :> True  :> False :> Nil
+    0x1 ->  False :> True  :> True  :> False :> False :> False :> False :> Nil
+    0x2 ->  True  :> True  :> False :> True  :> True  :> False :> True  :> Nil
+    0x3 ->  True  :> True  :> True  :> True  :> False :> False :> True  :> Nil
+    0x4 ->  False :> True  :> True  :> False :> False :> True  :> True  :> Nil
+    0x5 ->  True  :> False :> True  :> True  :> False :> True  :> True  :> Nil
+    0x6 ->  True  :> False :> True  :> True  :> True  :> True  :> True  :> Nil
+    0x7 ->  True  :> True  :> True  :> False :> False :> False :> False :> Nil
+    0x8 ->  True  :> True  :> True  :> True  :> True  :> True  :> True  :> Nil
+    0x9 ->  True  :> True  :> True  :> True  :> False :> True  :> True  :> Nil
+    0xa ->  True  :> True  :> True  :> False :> True  :> True  :> True  :> Nil
+    0xb ->  False :> False :> True  :> True  :> True  :> True  :> True  :> Nil
+    0xc ->  True  :> False :> False :> True  :> True  :> True  :> False :> Nil
+    0xd ->  False :> True  :> True  :> True  :> True  :> False :> True  :> Nil
+    0xe ->  True  :> False :> False :> True  :> True  :> True  :> True  :> Nil
+    0xf ->  True  :> False :> False :> False :> True  :> True  :> True  :> Nil
 
 showSS :: Vec 7 Bool -> String
 showSS (a :> b :> c :> d :> e :> f :> g :> Nil) = unlines . L.concat $
