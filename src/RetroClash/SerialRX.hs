23199b839e4d0333adc4021da9fd1be62999f575
Dr. ERDI Gergo <gergo@erdi.hu>
2019-10-05 23:11:03 +0200

    Rewrite SerialRX

--- src/RetroClash/SerialRX.hs
+++ src/RetroClash/SerialRX.hs
@@ -7,53 +7,50 @@ import Clash.Prelude
 import RetroClash.Utils
 import RetroClash.Clock
 
-import GHC.Generics (Generic, Generic1)
-import Control.DeepSeq
-
 import Control.Category ((>>>))
 import Control.Monad.State hiding (state)
 import Control.Monad.Trans.Writer
-import Data.Word
-import Data.Int
-import Data.Bits
+import Control.Monad.Trans.Maybe
 import Data.Maybe
 import Data.Monoid
-import Data.Proxy
+import Data.Foldable (for_)
 
 data RXState n = RXState
-    { buf :: Maybe Bit
-    , cnt :: Int
+    { cnt :: Int
+    , buf :: Maybe Bit
     , phase :: RXPhase n
     }
-    deriving (Generic, NFData, Show, NFDataX)
+    deriving (Generic, Show, NFDataX)
 
 data RXPhase n
     = Idle
-    | Start
-    | Bit (Index n) (Vec n Bit)
-    | Stop (Vec n Bit)
-    deriving (Generic, NFData, Show, NFDataX)
+    | StartBit
+    | DataBit (Index n) (Vec n Bit)
+    | StopBit (Vec n Bit)
+    deriving (Generic, Show, NFDataX)
 
 rx0 :: (KnownNat n) => Int -> Bit -> State (RXState n) (Maybe (Vec n Bit))
-rx0 halfPeriod bit = do
+rx0 halfPeriod input = fmap (getLast =<<) $ runMaybeT $ execWriterT $ do
     s@RXState{..} <- get
-    sampled <- do
-        let atMiddle = cnt == halfPeriod
-        modify $ \s -> if atMiddle then s{ cnt = 0, buf = Just bit } else s{ cnt = cnt + 1}
-        return $ if atMiddle then buf else Nothing
-
-    fmap getLast $ execWriterT $ case phase of
-        Idle    | bit == low -> goto Start
-        Start   | Just read <- sampled -> goto $ if read == low then Bit 0 (repeat low) else Idle
-        Bit i x | Just read <- sampled -> do
-            let (x', _) = shiftInFromLeft read x
-            goto $ maybe Stop Bit (succIdx i) x'
-        Stop x  | Just read <- sampled -> do
-            when (read == high) $ tell $ Last . Just $ x
+    let sample = do
+            let atMiddle = cnt == halfPeriod
+            put $ if atMiddle then s{ cnt = 0, buf = Just input } else s{ cnt = cnt + 1 }
+            return $ if atMiddle then buf else Nothing
+    case phase of
+        Idle -> when (input == low) $ goto StartBit
+        StartBit -> do
+            Just bit <- sample
+            goto $ if bit == low then DataBit 0 (repeat low) else Idle
+        DataBit i x -> do
+            Just bit <- sample
+            let (x', _) = shiftInFromLeft bit x
+            goto $ maybe StopBit DataBit (succIdx i) x'
+        StopBit x -> do
+            Just bit <- sample
+            when (bit == high) $ tell $ Last . Just $ x
             goto Idle
-        _ -> return ()
   where
-    goto ph = modify $ \s -> s{ cnt = 0, buf = Nothing, phase = ph }
+    goto ph = put RXState{ cnt = 0, buf = Nothing, phase = ph }
 
 serialRX
     :: forall n rate dom. (KnownNat n, KnownNat rate, KnownNat (ClockDivider dom (HzToPeriod (rate * 2))))
