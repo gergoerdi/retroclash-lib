0ca4b39e3d5b83f59f36a40197451ea6d206bad3
Dr. ERDI Gergo <gergo@erdi.hu>
2020-08-23 14:52:20 +0800

    Add generic support for CPUs that can wait for input

--- src/RetroClash/CPU.hs
+++ src/RetroClash/CPU.hs
@@ -10,6 +10,7 @@ import Data.Functor.Identity
 
 import Control.Monad.Writer
 import Control.Monad.State
+import Control.Monad.Trans.Maybe
 import Control.Lens (Setter', scribe, iso)
 
 import Barbies
@@ -23,10 +24,18 @@ assignOut :: (Applicative f, MonadWriter (Barbie b f) m) => Setter' (b f) (f a) 
 assignOut fd x = fd .:= x
 
 update :: (BareB b, ApplicativeB (b Covered)) => Pure b -> Partial b -> Pure b
-update initials edits = bstrip $ bzipWith update1 (bcover initials) (getBarbie edits)
+update initials = bstrip . bzipWith update1 (bcover initials) . getBarbie
   where
     update1 :: Identity a -> Last a -> Identity a
     update1 initial edit = maybe initial Identity (getLast edit)
+
+type Mask b = b Covered (Const Bool)
+
+keep :: (BareB b, ApplicativeB (b Covered), Monoid (Partial b)) => Mask b -> Partial b -> Partial b
+keep mask = Barbie . bzipWith3 keep1 mask (getBarbie mempty) . getBarbie
+  where
+    keep1 :: Const Bool a -> f a -> f a -> f a
+    keep1 (Const mask) empty x = if mask then x else empty
 
 type CPUM s o = WriterT (Barbie (o Covered) Last) (State s)
 
@@ -41,6 +50,33 @@ mealyCPU
     -> Signals dom i -> Signals dom o
 mealyCPU initState defaultOutput step =
     bunbundle . mealyState (runCPU defaultOutput . step) initState . bbundle
+
+stallable
+    :: (BareB o, ApplicativeB (o Covered), Monoid (Partial o))
+    => Mask o
+    -> MaybeT (CPUM s o) ()
+    -> State (s, Partial o) ()
+stallable mask step = modify $ \(s0, out0) -> case runState (runWriterT . runMaybeT $ step) s0 of
+    ((Nothing, _), _) -> (s0, keep mask out0)
+    ((Just (), out), s) -> (s, out)
+
+mooreCPU
+    :: (BareB i, TraversableB (i Covered))
+    => (NFDataX s, NFDataX (Partial o), Monoid (Partial o))
+    => (BareB o, ApplicativeB (o Covered), DistributiveB (o Covered))
+    => (HiddenClockResetEnable dom)
+    => s
+    -> (s -> Pure o)
+    -> Mask o
+    -> (Pure i -> MaybeT (CPUM s o) ())
+    -> Signals dom i -> Signals dom o
+mooreCPU initState defaultOutput mask step =
+    bunbundle .
+    mooreState
+      (stallable mask . step)
+      (\(s, o) -> update (defaultOutput s) o)
+      (initState, mempty) .
+    bbundle
 
 runCPU
     :: (BareB o, ApplicativeB (o Covered))
