fc502ca2ff1e9f01fd7b2a0a6ca569b2351f70c0
Dr. ERDI Gergo <gergo@erdi.hu>
2020-10-04 17:54:11 +0800

    Add RetroClash.Port

---
+++ src/RetroClash/Port.hs
@@ -0,0 +1,65 @@
+{-# LANGUAGE LambdaCase #-}
+module RetroClash.Port
+    ( PortCommand(..)
+    , maskAddr
+    , basedAt
+    ) where
+
+import Clash.Prelude
+import Clash.Annotations.TH
+
+import Control.Monad (guard)
+import Data.Bifunctor
+import Data.Bifoldable
+import Data.Bitraversable
+import Control.Monad
+
+data PortCommand port a
+    = ReadPort port
+    | WritePort port a
+    deriving (Generic, NFDataX, Show)
+
+instance Functor (PortCommand port) where
+    {-# INLINE fmap #-}
+    fmap f = \case
+        ReadPort port -> ReadPort port
+        WritePort port val -> WritePort port (f val)
+
+instance Bifunctor PortCommand where
+    {-# INLINE bimap #-}
+    bimap f g = \case
+        ReadPort port -> ReadPort (f port)
+        WritePort port val -> WritePort (f port) (g val)
+
+    {-# INLINE second #-}
+    second = fmap
+
+instance Bifoldable PortCommand where
+    {-# INLINE bifoldMap #-}
+    bifoldMap f g = \case
+        ReadPort port -> f port
+        WritePort port val -> f port <> g val
+
+instance Bitraversable PortCommand where
+    {-# INLINE bitraverse #-}
+    bitraverse f g = \case
+        ReadPort port -> ReadPort <$> f port
+        WritePort port val -> WritePort <$> f port <*> g val
+
+maskAddr :: forall n k. (KnownNat n, KnownNat k) => Unsigned (n + k) -> Unsigned (n + k) -> Maybe (Unsigned k)
+maskAddr base0 addr = do
+    let (space, offset) = split addr
+    guard $ space == base
+    return offset
+  where
+    (base, _) = split base0
+
+    split :: Unsigned (n + k) -> (Unsigned n, Unsigned k)
+    split = bitCoerce
+
+basedAt
+    :: forall n k a. (KnownNat n, KnownNat k)
+    => Unsigned (n + k)
+    -> Maybe (PortCommand (Unsigned (n + k)) a)
+    -> Maybe (PortCommand (Unsigned k) a)
+basedAt base cmd = bitraverse (maskAddr base) pure =<< cmd
