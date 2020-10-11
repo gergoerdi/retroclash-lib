c25cda89de6cdb61d6c463d62d102b8da5c0d4a4
Dr. ERDI Gergo <gergo@erdi.hu>
2020-10-11 15:37:40 +0800

    Simplify `Addressing`, since read and write type should always be the same

--- src/RetroClash/Memory.hs
+++ src/RetroClash/Memory.hs
@@ -24,25 +24,21 @@ import Control.Monad.Writer
 type RAM dom a d = Signal dom a -> Signal dom (Maybe (a, d)) -> Signal dom d
 type ROM dom a d = Signal dom a ->                              Signal dom d
 
-data Mem dom a d
-    = RAM (Signal dom a -> Signal dom (Maybe (a, d)) -> Signal dom d)
-    | ROM (Signal dom a ->                              Signal dom d)
-
 packRam :: (BitPack d) => RAM dom a (BitVector (BitSize d)) -> RAM dom a d
 packRam ram addr = fmap unpack . ram addr . fmap (second pack <$>)
 
-newtype CollectedSignal dom a = CollectedSignal{ getCollectedSignal :: Signal dom (Maybe a) }
+newtype FirstSignal dom a = FirstSignal{ getFirstSignal :: Signal dom (First a) }
 
-instance Semigroup (CollectedSignal dom a) where
-    x <> y = CollectedSignal $ mplus <$> getCollectedSignal x <*> getCollectedSignal y
+instance Semigroup (FirstSignal dom a) where
+    x <> y = FirstSignal $ (<>) <$> getFirstSignal x <*> getFirstSignal y
 
-instance Monoid (CollectedSignal dom a) where
-    mempty = CollectedSignal $ pure Nothing
+instance Monoid (FirstSignal dom a) where
+    mempty = FirstSignal $ pure mempty
 
-newtype Addressing dom addr dat res a = Addressing
+newtype Addressing dom addr dat a = Addressing
     { unAddressing :: ReaderT
                       (Signal dom (Maybe addr), Signal dom (Maybe dat))
-                      (Writer (CollectedSignal dom res))
+                      (Writer (FirstSignal dom dat))
                       a
     }
     deriving newtype (Functor, Applicative, Monad)
@@ -50,32 +46,34 @@ newtype Addressing dom addr dat res a = Addressing
 memoryMap
     :: Signal dom (Maybe addr)
     -> Signal dom (Maybe dat)
-    -> Addressing dom addr dat res out
-    -> (Signal dom (Maybe res), out)
-memoryMap addr write spec = (result, output)
+    -> Addressing dom addr dat out
+    -> (Signal dom (Maybe dat), out)
+memoryMap addr write spec = (getFirst <$> getFirstSignal result, output)
   where
-    (output, getCollectedSignal -> result) = runWriter $ runReaderT (unAddressing spec) (addr, write)
+    (output, result) = runWriter $ runReaderT (unAddressing spec) (addr, write)
 
 memoryMap_
     :: Signal dom (Maybe addr)
     -> Signal dom (Maybe dat)
-    -> Addressing dom addr dat res ()
-    -> Signal dom (Maybe res)
+    -> Addressing dom addr dat ()
+    -> Signal dom (Maybe dat)
 memoryMap_ addr write spec = fst $ memoryMap addr write spec
 
 mapAddr
     :: (HiddenClockResetEnable dom)
-    => (addr -> Maybe addr') -> Addressing dom addr' dat out r -> Addressing dom addr dat out r
+    => (addr -> Maybe addr') -> Addressing dom addr' dat r -> Addressing dom addr dat r
 mapAddr f body = Addressing $ withReaderT (first $ fmap (f =<<)) $ do
     (addr, _) <- ask
-    censor (CollectedSignal . fmap join . enable (delay False $ isJust <$> addr) . getCollectedSignal) $ unAddressing body
+    censor (FirstSignal . whenAddr addr . getFirstSignal) $ unAddressing body
+  where
+    whenAddr addr sig = mux (delay False $ isJust <$> addr) sig (pure mempty)
 
 infix 1 <||>
 (<||>)
     :: (HiddenClockResetEnable dom)
-    => Addressing dom addr1 dat out r1
-    -> Addressing dom addr2 dat out r2
-    -> Addressing dom (Either addr1 addr2) dat out (r1, r2)
+    => Addressing dom addr1 dat r1
+    -> Addressing dom addr2 dat r2
+    -> Addressing dom (Either addr1 addr2) dat (r1, r2)
 body1 <||> body2 = do
     x <- mapAddr (either Just (const Nothing)) body1
     y <- mapAddr (either (const Nothing) Just) body2
@@ -85,38 +83,38 @@ mask
     :: (KnownNat k, KnownNat n)
     => (HiddenClockResetEnable dom)
     => Unsigned (n + k)
-    -> Addressing dom (Unsigned k)       dat out r
-    -> Addressing dom (Unsigned (n + k)) dat out r
+    -> Addressing dom (Unsigned k)       dat r
+    -> Addressing dom (Unsigned (n + k)) dat r
 mask base = mapAddr (maskAddr base)
 
 offset
     :: (Num addr, Ord addr)
     => (HiddenClockResetEnable dom)
     => addr
-    -> Addressing dom addr dat out r
-    -> Addressing dom addr dat out r
+    -> Addressing dom addr dat r
+    -> Addressing dom addr dat r
 offset base = mapAddr $ \addr -> do
     guard $ base <= addr
     return $ addr - base
 
-ram :: (Num addr) => RAM dom addr dat -> Addressing dom addr dat dat ()
+ram :: (Num addr) => RAM dom addr dat -> Addressing dom addr dat ()
 ram mkRam = Addressing $ do
     (addr, w) <- ask
     let output = mkRam (fromMaybe 0 <$> addr) (liftA2 (,) <$> addr <*> w)
-    tell $ CollectedSignal $ Just <$> output
+    tell $ FirstSignal $ pure <$> output
 
-rom :: (Num addr) => ROM dom addr dat -> Addressing dom addr w dat ()
+rom :: (Num addr) => ROM dom addr dat -> Addressing dom addr dat ()
 rom mkRom = Addressing $ do
     (addr, w) <- ask
     let output = mkRom (fromMaybe 0 <$> addr)
-    tell $ CollectedSignal $ Just <$> output
+    tell $ FirstSignal $ pure <$> output
 
 port
-    :: (HiddenClockResetEnable dom, NFDataX out)
-    => (Signal dom (Maybe (PortCommand addr dat)) -> (Signal dom (Maybe out), r))
-    -> Addressing dom addr dat out r
+    :: (HiddenClockResetEnable dom, NFDataX dat)
+    => (Signal dom (Maybe (PortCommand addr dat)) -> (Signal dom (Maybe dat), r))
+    -> Addressing dom addr dat r
 port mkPort = Addressing $ do
     (addr, w) <- ask
     let (output, result) = mkPort $ portFromAddr addr w
-    tell $ CollectedSignal $ delay Nothing output
+    tell $ FirstSignal $ delay mempty $ First <$> output
     return result
