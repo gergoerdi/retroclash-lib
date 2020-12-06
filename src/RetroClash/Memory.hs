a304e25567a581f57770d77541e62fa0f78ff952
Dr. ERDI Gergo <gergo@erdi.hu>
2020-12-06 21:19:23 +0800

    RecursiveDo is exactly the kind of bullshit we don't need on top of all the other bullshit

--- src/RetroClash/Memory.hs
+++ src/RetroClash/Memory.hs
@@ -1,5 +1,5 @@
 {-# LANGUAGE DerivingStrategies, GeneralizedNewtypeDeriving #-}
-{-# LANGUAGE RankNTypes, RecursiveDo #-}
+{-# LANGUAGE RankNTypes #-}
 module RetroClash.Memory
     ( memoryMap, memoryMap_
 
@@ -98,8 +98,12 @@ readWrite
     => (Signal dom (Maybe addr') -> Signal dom (Maybe dat) -> (Signal dom (Maybe dat), a))
     -> Addressing s dom dat addr (Component s addr', a)
 readWrite mkComponent = Addressing $ do
-    rec (component, addr, wr) <- unAddressing $ conduit read
-        let (read, x) = mkComponent addr wr
+    component <- Component theType <$> get <* modify succ
+    (_, wr, addrs) <- ask
+    let addr = firstIn . fromMaybe mempty $ DMap.lookup component (addrMap addrs)
+        selected = isJust <$> addr
+    let (read, x) = mkComponent addr wr
+    tell (gated (delay False selected) (fanIn read), mempty)
     return (component, x)
 
 {-# INLINE readWrite_ #-}
