24f412e456ee00badcf482ebb90e6ed125ca16ef
Gergo ERDI <gergo@erdi.hu>
2022-04-19 21:46:33 +0800

    Latch outgoing message contents when starting a new message

--- src/RetroClash/I2C.hs
+++ src/RetroClash/I2C.hs
@@ -8,20 +8,20 @@ import RetroClash.Clock
 import Control.Monad.State
 import Data.Maybe (isJust, isNothing)
 
-type Message = (Unsigned 8, Unsigned 8, Unsigned 8)
+type Message = (BitVector 8, BitVector 8, BitVector 8)
 
 data MessageState
-    = Init Init
-    | SendAddr (SendBits 8)
-    | SendSubaddr (SendBits 8)
-    | SendDat (SendBits 8)
-    | Teardown Teardown
+    = Init        (BitVector 8, BitVector 8, BitVector 8) Init
+    | SendAddr    (BitVector 8, BitVector 8)              (SendBits 8)
+    | SendSubaddr (BitVector 8)                           (SendBits 8)
+    | SendDat                                             (SendBits 8)
+    | Teardown                                            Teardown
     deriving (Show, Generic, BitPack, NFDataX)
 
 data SendBits n
-  = SendBit SendTransition (Index n)
-  | SendAck SendTransition
-  deriving (Show, Generic, NFDataX)
+    = SendBit SendTransition (BitVector n) (Index n)
+    | SendAck SendTransition
+    deriving (Show, Generic, NFDataX)
 deriving instance (KnownNat n, 1 <= n) => BitPack (SendBits n)
 
 data SendTransition = SDASet | Tick
@@ -33,44 +33,43 @@ data Init = StartInit | SDALow | SCLLow
 data Teardown = StartTeardown | SCLHigh | SDAHigh
   deriving (Show, Enum, Bounded, Eq, Generic, BitPack, NFDataX)
 
-startBit :: (KnownNat n) => SendBits n
-startBit = SendBit minBound maxBound
+startBit :: (KnownNat n) => BitVector n -> SendBits n
+startBit xs = SendBit minBound xs minBound
 
-nextBit :: (KnownNat n) => SendBits n -> Maybe (SendBits n)
-nextBit (SendBit transition i) = Just $ case succIdx transition of
-    Just transition' -> SendBit transition' i
-    Nothing -> maybe (SendAck minBound) (SendBit minBound) $ predIdx i
-nextBit (SendAck transition) = SendAck <$> succIdx transition
+succBit :: (KnownNat n) => SendBits n -> Maybe (SendBits n)
+succBit (SendBit transition xs i) = Just $ case succIdx transition of
+    Just transition' -> SendBit transition' xs i
+    Nothing -> maybe (SendAck minBound) (SendBit minBound (xs `shiftL` 1)) $ succIdx i
+succBit (SendAck transition) = SendAck <$> succIdx transition
 
-shiftOut :: (KnownNat n) => Unsigned n -> SendBits n -> (Maybe Bit, Maybe Bit)
-shiftOut x (SendBit transition i) = (Just $ boolToBit $ transition == Tick, Just $ x ! i)
-shiftOut _ (SendAck transition) = (Just $ boolToBit $ transition == Tick, Nothing)
+shiftOut :: (KnownNat n) => SendBits n -> (Maybe Bit, Maybe Bit)
+shiftOut (SendBit transition xs i) = (Just $ boolToBit $ transition == Tick, Just $ msb xs)
+shiftOut (SendAck transition) = (Just $ boolToBit $ transition == Tick, Nothing)
 
 -- We only drive clk (clock stretching not implemented), and we never query
 -- peripherals over I2C, so we never actually use sdaIn and sclIn
-i2cNext :: Bool -> Bit -> Bit -> Maybe MessageState -> Maybe MessageState
-i2cNext startNew _sdaIn _sclIn = \case
-    Nothing              -> Init StartInit <$ guard startNew
+i2cNext :: Maybe Message -> Bit -> Bit -> Maybe MessageState -> Maybe MessageState
+i2cNext newMsg _sdaIn _sclIn = \case
+    Nothing                                     -> Init <$> newMsg <*> pure StartInit
 
-    Just (Init ramp)     -> Just $ maybe (SendAddr startBit) Init $ succIdx ramp
+    Just (Init        xss@(xs1, xs2, xs3) ramp) -> Just $ maybe (SendAddr    (xs2, xs3) (startBit xs1)) (Init        xss) $ succIdx ramp
+    Just (SendAddr    xss@(xs2, xs3)      b)    -> Just $ maybe (SendSubaddr xs3        (startBit xs2)) (SendAddr    xss) $ succBit b
+    Just (SendSubaddr xss@xs3             b)    -> Just $ maybe (SendDat                (startBit xs3)) (SendSubaddr xss) $ succBit b
+    Just (SendDat                         b)    -> Just $ maybe (Teardown StartTeardown)                SendDat           $ succBit b
 
-    Just (SendAddr b)    -> Just $ maybe (SendSubaddr startBit) SendAddr $ nextBit b
-    Just (SendSubaddr b) -> Just $ maybe (SendDat startBit) SendSubaddr $ nextBit b
-    Just (SendDat b)     -> Just $ maybe (Teardown StartTeardown) SendDat $ nextBit b
+    Just (Teardown ramp)                        -> Teardown <$> succIdx ramp
 
-    Just (Teardown ramp) -> Teardown <$> succIdx ramp
-
-i2cOutput :: Message -> Maybe MessageState -> (Maybe Bit, Maybe Bit)
-i2cOutput (addr, subaddr, dat) = \case
+i2cOutput :: Maybe MessageState -> (Maybe Bit, Maybe Bit)
+i2cOutput = \case
     Nothing                       -> (Just 1, Just 1)
 
-    Just (Init StartInit)         -> (Just 1, Just 1)
-    Just (Init SDALow)            -> (Just 1, Just 0)
-    Just (Init SCLLow)            -> (Just 0, Just 0)
+    Just (Init _ StartInit)       -> (Just 1, Just 1)
+    Just (Init _ SDALow)          -> (Just 1, Just 0)
+    Just (Init _ SCLLow)          -> (Just 0, Just 0)
 
-    Just (SendAddr b)             -> shiftOut addr b
-    Just (SendSubaddr b)          -> shiftOut subaddr b
-    Just (SendDat b)              -> shiftOut dat b
+    Just (SendAddr _ b)           -> shiftOut b
+    Just (SendSubaddr _ b)        -> shiftOut b
+    Just (SendDat b)              -> shiftOut b
 
     Just (Teardown StartTeardown) -> (Just 0, Just 0)
     Just (Teardown SCLHigh)       -> (Just 1, Just 0)
@@ -99,8 +98,8 @@ i2cMaster i2cRate@SNat msg sclIn sdaIn = (sclOut, sdaOut, ready)
     step :: (Bool, Maybe Message, Bit, Bit) -> State (Maybe MessageState) (Maybe Bit, Maybe Bit, Bool)
     step (tick, msg, sclIn, sdaIn) = do
         s <- get
-        when tick $ modify $ i2cNext (isJust msg) sdaIn sclIn
+        when tick $ modify $ i2cNext msg sdaIn sclIn
         s' <- get
         let ready = tick && isNothing s'
-            (sclOut, sdaOut) = i2cOutput (fromJustX msg) s
+            (sclOut, sdaOut) = i2cOutput s
         return (sclOut, sdaOut, ready)
